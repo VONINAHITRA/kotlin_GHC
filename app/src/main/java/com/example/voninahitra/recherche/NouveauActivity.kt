be3ad74813f5c3b1d6c617c68ffffec7b8cf7489
VONINAHITRA <voninahitracalvyn@gmail.com>
2020-11-23 13:51:56 +0300

    Recherche elastique avec kotlin

---
+++ app/src/main/java/com/example/voninahitra/recherche/NouveauActivity.kt
@@ -0,0 +1,12 @@
+package com.example.voninahitra.recherche
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NouveauActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nouveau)
+    }
+}
---
+++ app/src/main/java/com/example/voninahitra/recherche/PublicationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.voninahitra.recherche
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PublicationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_publication)
+    }
+}
